a71f7f9ac28ab168d1f58570af2c84bd059ac383
pedrovargas00 <01vargaspedro@gmail.com>
2020-02-19 22:12:41 -0600

    Inicio

--- Parcial_1/Query.java
+++ Parcial_1/Query.java
@@ -6,4 +6,38 @@ class Query{
 
   }
 
+  public void consulta(String linea){
+
+
+  }
+
+  public ArrayList<String> divide(BufferedReader bf)throws IOException {
+
+    ArrayList<String> entrada = new ArrayList();
+    String[] in;
+    String auxiliar;
+
+    while((auxiliar = bf.readLine()) != null){
+      in = auxiliar.split(",");
+      for(int i = 0; i < in.length; i++)
+        entrada.add(in[i]);
+    }
+
+    return entrada;
+  }
+
+  public void read(String nombre)throws IOException, FileNotFoundException {
+
+    BufferedReader bf = new BufferedReader(new FileReader(nombre));
+    ArrayList<String> entrada = new ArrayList();
+    int j = 0;
+
+    if(bf.ready() == false){
+      System.out.println("El archivo está vacío");
+    }
+
+    entrada.addAll(divide(bf));
+
+    }
+
 }
